4bd88b09aeb1932c250d1110f1b2f022deefa646
femmyte <sanyaoluadefemi@gmail.com>
2021-12-28 15:51:39 +0800

    added the nav and the body component

--- src/data.js
+++ src/data.js
@@ -1,5 +1,11 @@
 import one from './img/1.jpg'
 import two from './img/2.jpg'
+import three from './img/3.jpg'
+import four from './img/4.png'
+import five from './img/5.jpg'
+import seven from './img/7.jpg'
+import six from './img/6.jpg'
+
 const data = [
   {
     id: 1,
@@ -14,60 +20,60 @@ const data = [
   {
     id: 1,
     title: 'Iba',
-    img: one,
+    img: three,
   },
   {
-    id: 2,
+    id: 3,
+    title: 'Aseda',
+    img: four,
+  },
+  {
+    id: 4,
+    title: 'Iba',
+    img: five,
+  },
+  {
+    id: 5,
+    title: 'Aseda',
+    img: six,
+  },
+  {
+    id: 7,
+    title: 'Iba',
+    img: seven,
+  },
+  {
+    id: 8,
     title: 'Aseda',
     img: two,
   },
   {
-    id: 1,
+    id: 9,
     title: 'Iba',
     img: one,
   },
   {
-    id: 2,
+    id: 10,
     title: 'Aseda',
     img: two,
   },
   {
-    id: 1,
+    id: 11,
     title: 'Iba',
     img: one,
   },
   {
-    id: 2,
+    id: 12,
     title: 'Aseda',
     img: two,
   },
   {
-    id: 1,
+    id: 13,
     title: 'Iba',
     img: one,
   },
   {
-    id: 2,
-    title: 'Aseda',
-    img: two,
-  },
-  {
-    id: 1,
-    title: 'Iba',
-    img: one,
-  },
-  {
-    id: 2,
-    title: 'Aseda',
-    img: two,
-  },
-  {
-    id: 1,
-    title: 'Iba',
-    img: one,
-  },
-  {
-    id: 2,
+    id: 14,
     title: 'Aseda',
     img: two,
   },
